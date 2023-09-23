e7274a2bd309a4809fa93060b1131be0cbd4a6ce
Egor Kirin <egorcom27@gmail.com>
2023-09-23 23:04:56 +0300

    анимация кнопки создания пиццы

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+
+const buttonVariants = {
+  visible: {
+    x: [0, -10, 10, -10, 10, 0],
+    transition: { delay: 2 }
+  },
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
 
 const Home = () => {
   return (
@@ -8,20 +23,18 @@ const Home = () => {
       className="home container"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ delay: 0.4, duration: 1.5 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
     >
-      <h2>Добро пожаловать в пиццерию</h2>
-      <NavLink to="/base">
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >
-          Создай свою пиццу
+          Create Your Pizza
         </motion.button>
-      </NavLink>
+      </Link>
     </motion.div>
   );
 };
